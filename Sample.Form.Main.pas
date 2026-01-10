8eaf144dde41f3ab1d4e26bec604d3ea0754eac9
Lara Miriam Tamy del Fabro <133981326+LaMitaOne@users.noreply.github.com>
2026-01-10 12:16:33 +0100

    Add files via upload

--- Sample.Form.Main.pas
+++ Sample.Form.Main.pas
@@ -9,7 +9,8 @@ uses
   FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Effects,
   uSkFlowmotion,
   { Skia }
-  System.Skia, FMX.Skia, System.ImageList, FMX.ImgList, FMX.ListBox;
+  System.Skia, FMX.Skia, System.ImageList, FMX.ImgList, FMX.ListBox, FMX.Colors,
+  FMX.Edit, FMX.EditBox, FMX.SpinBox;
 
 type
   { TfrmMain }
@@ -29,7 +30,6 @@ type
     OpenDialog1: TOpenDialog;
     ImageList1: TImageList;
     Label1: TLabel;
-    ComboBox1: TComboBox;
     CheckBox1: TCheckBox;
     Rectangle1: TRectangle;
     CheckBox2: TCheckBox;
@@ -43,6 +43,34 @@ type
     CheckBox6: TCheckBox;
     CheckBox7: TCheckBox;
     CheckBox8: TCheckBox;
+    Timer1: TTimer;
+    ColorPicker1: TColorPicker;
+    rbselfontcol: TRadioButton;
+    rbfontcol: TRadioButton;
+    rbselcapback: TRadioButton;
+    rbcapback: TRadioButton;
+    rbglowcol: TRadioButton;
+    rbhotcol: TRadioButton;
+    rbrotatedot: TRadioButton;
+    CheckBox9: TCheckBox;
+    SpinBox1: TSpinBox;
+    rbAnimspeed: TRadioButton;
+    rbGlowwidth: TRadioButton;
+    rbHotwidth: TRadioButton;
+    rbFontsize: TRadioButton;
+    rbCaptionAlpha: TRadioButton;
+    rbCaptionYOffset: TRadioButton;
+    rbPagesize: TRadioButton;
+    ComboBox1: TComboBox;
+    CheckBox10: TCheckBox;
+    CheckBox11: TCheckBox;
+    rbstartingAngle: TRadioButton;
+    rbRoundEdges: TRadioButton;
+    Panel2: TPanel;
+    rbrotatedothot: TRadioButton;
+    rbrotatedotdown: TRadioButton;
+    CheckBox12: TCheckBox;
+    rbrotateall: TRadioButton;
     procedure Button10Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
@@ -53,6 +81,9 @@ type
     procedure Button7Click(Sender: TObject);
     procedure Button8Click(Sender: TObject);
     procedure Button9Click(Sender: TObject);
+    procedure CheckBox10Change(Sender: TObject);
+    procedure CheckBox11Change(Sender: TObject);
+    procedure CheckBox12Change(Sender: TObject);
     procedure CheckBox1Change(Sender: TObject);
     procedure CheckBox2Change(Sender: TObject);
     procedure CheckBox3Change(Sender: TObject);
@@ -61,15 +92,23 @@ type
     procedure CheckBox6Change(Sender: TObject);
     procedure CheckBox7Change(Sender: TObject);
     procedure CheckBox8Change(Sender: TObject);
+    procedure CheckBox9Change(Sender: TObject);
+    procedure ColorPicker1MouseUp(Sender: TObject; Button: TMouseButton; Shift:
+        TShiftState; X, Y: Single);
     procedure ComboBox1Change(Sender: TObject);
     procedure fanFadeOutTransitionFinish(Sender: TObject);
     procedure saiAnimatedLogoAnimationFinished(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure lytcontrolsResize(Sender: TObject);
+    procedure rbPagesizeMouseDown(Sender: TObject; Button: TMouseButton; Shift:
+        TShiftState; X, Y: Single);
+    procedure SpinBox1Change(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
     skfmFlowGallery: TSkFlowmotion;
     procedure InitGallery;
     procedure MouseWheel(Shift: TShiftState; WheelDelta: Integer; var Handled: Boolean); override;
+    procedure Flowmotion1SelectedImageEnterZone(Sender: TObject; ImageItem: TImageItem; const ZoneName: string);
   public
     { Public declarations }
   end;
@@ -84,6 +123,13 @@ implementation
 procedure TfrmMain.Button10Click(Sender: TObject);
 begin
    skfmFlowGallery.Clear(true, true, Panel1.BoundsRect.Round, Panel1.BoundsRect.Round, iesFromPoint, true);
+end;
+
+procedure TfrmMain.Flowmotion1SelectedImageEnterZone(Sender: TObject; ImageItem: TImageItem; const ZoneName: string);
+begin
+  // Show a message or perform an action based on the zone name, here u can do...whatever u like then
+  if ZoneName = 'ActivationZone 1' then
+    ShowMessage('Image entered ActivationZone 1! ');
 end;
 
 procedure TfrmMain.MouseWheel(Shift: TShiftState; WheelDelta: Integer; var Handled: Boolean);
@@ -113,8 +159,21 @@ end;
 
 procedure TfrmMain.Button4Click(Sender: TObject);
 begin
-  skfmFlowGallery.ImageEntryStyle := iesFromLeft;
+  //skfmFlowGallery.ImageEntryStyle := iesFromLeft;
+  //skfmFlowGallery.AddImageAsync(ExtractFilePath(ParamStr(0)) + inttostr(random(13)+1) + '.jpg');
+
+  skfmFlowGallery.ImageEntryStyle := iesFromPoint;
+  skfmFlowGallery.EntryPoint := Point(Round(Button5.Position.X), Round(Button5.Position.Y));
   skfmFlowGallery.AddImageAsync(ExtractFilePath(ParamStr(0)) + inttostr(random(13)+1) + '.jpg');
+end;
+
+procedure TfrmMain.Button9Click(Sender: TObject);
+begin
+  skfmFlowGallery.ImageEntryStyle := iesFromPoint;
+  skfmFlowGallery.EntryPoint := Point(Round(Button9.Position.X), Round(Button9.Position.Y));
+   if Opendialog1.Execute then begin
+    skfmFlowGallery.AddImageAsync(Opendialog1.FileName);
+   end;
 end;
 
 procedure TfrmMain.Button5Click(Sender: TObject);
@@ -135,18 +194,19 @@ begin
   Captionlist:= TStringList.create;
   Hintlist:= TStringList.create;
   try
-   for i := 0 to 14 do begin
-     IMList.add(AppDir + inttostr(i) + '.jpg');
-     Pathlist.add('Folder or whatever');
-     Captionlist.add('Caption');
-     Hintlist.Add('Hint');
-   end;
    for i := 14 downto 0 do begin
      IMList.add(AppDir + inttostr(i) + '.jpg');
      Pathlist.add('Folder or whatever');
      Captionlist.add('Caption');
      Hintlist.Add('Hint');
    end;
+   for i := 0 to 14 do begin
+     IMList.add(AppDir + inttostr(i) + '.jpg');
+     Pathlist.add('Folder or whatever');
+     Captionlist.add('Caption');
+     Hintlist.Add('Hint');
+   end;
+
   skfmFlowGallery.MaxZoomSize := trunc(Clientwidth / 4);
   skfmFlowGallery.AddImagesAsync(IMList,Captionlist,Pathlist, Hintlist);
   finally
@@ -167,12 +227,21 @@ begin
   skfmFlowGallery.ResetAllRotations;
 end;
 
-procedure TfrmMain.Button9Click(Sender: TObject);
+
+
+procedure TfrmMain.CheckBox10Change(Sender: TObject);
 begin
-  skfmFlowGallery.ImageEntryStyle := iesFromLeft;
-   if Opendialog1.Execute then begin
-    skfmFlowGallery.AddImageAsync(Opendialog1.FileName);
-   end;
+ skfmFlowGallery.HotTrackZoom := CheckBox10.isChecked;
+end;
+
+procedure TfrmMain.CheckBox11Change(Sender: TObject);
+begin
+  skfmFlowGallery.BreathingEnabled := CheckBox11.isChecked;
+end;
+
+procedure TfrmMain.CheckBox12Change(Sender: TObject);
+begin
+ skfmFlowGallery.ZoomSelectedtoCenter := Checkbox12.IsChecked;
 end;
 
 procedure TfrmMain.CheckBox1Change(Sender: TObject);
@@ -194,7 +263,7 @@ end;
 procedure TfrmMain.CheckBox4Change(Sender: TObject);
 begin
 if CheckBox4.IsChecked then
-   skfmFlowGallery.StartingAngle := 0 else skfmFlowGallery.StartingAngle := -1;
+   skfmFlowGallery.StartingAngle := -1 else skfmFlowGallery.StartingAngle := 0;
 end;
 
 procedure TfrmMain.CheckBox5Change(Sender: TObject);
@@ -215,6 +284,36 @@ end;
 procedure TfrmMain.CheckBox8Change(Sender: TObject);
 begin
   skfmFlowGallery.CaptionOnHoverOnly := CheckBox8.IsChecked;
+end;
+
+procedure TfrmMain.CheckBox9Change(Sender: TObject);
+begin
+  skfmFlowGallery.ShowSmallPicOnlyOnHover := CheckBox9.IsChecked;
+end;
+
+procedure TfrmMain.ColorPicker1MouseUp(Sender: TObject; Button: TMouseButton;
+    Shift: TShiftState; X, Y: Single);
+begin
+if not Assigned(skfmFlowGallery) then Exit;
+
+  if rbselfontcol.IsChecked then
+    skfmFlowGallery.SelectedCaptionColor := ColorPicker1.Color
+  else if rbfontcol.IsChecked then
+    skfmFlowGallery.CaptionColor := ColorPicker1.Color
+  else if rbselcapback.IsChecked then
+    skfmFlowGallery.SelectedCaptionBackground := ColorPicker1.Color
+  else if rbcapback.IsChecked then
+    skfmFlowGallery.CaptionBackground := ColorPicker1.Color
+  else if rbglowcol.IsChecked then
+    skfmFlowGallery.GlowColor := ColorPicker1.Color
+  else if rbhotcol.IsChecked then
+    skfmFlowGallery.HotTrackColor := ColorPicker1.Color
+  else if rbrotatedot.IsChecked then
+    skfmFlowGallery.RotateDotColor := ColorPicker1.Color
+  else if rbrotatedothot.IsChecked then
+    skfmFlowGallery.RotateDotHotColor := ColorPicker1.Color
+  else if rbrotatedotDown.IsChecked then
+    skfmFlowGallery.RotateDotDownColor := ColorPicker1.Color;
 end;
 
 procedure TfrmMain.ComboBox1Change(Sender: TObject);
@@ -249,26 +348,15 @@ begin
   skfmFlowGallery.ShowHint := true;
   skfmFlowGallery.SmallPicVisible := True;
   skfmFlowGallery.SmallPicImageList := Imagelist1;
+  skfmFlowGallery.OnSelectedImageEnterZone := Flowmotion1SelectedImageEnterZone;
+  skfmFlowGallery.AddActivationZone('ActivationZone 1', Panel1.BoundsRect);
+
   IMList:= TStringList.create;
   Pathlist:= TStringList.create;
   Captionlist:= TStringList.create;
   Hintlist:= TStringList.create;
   smallimgindex := TList.Create;
   try
-   for i := 0 to 14 do begin
-     IMList.add(AppDir + inttostr(i) + '.jpg');
-     Pathlist.add('Folder or whatever');
-     Captionlist.add('Caption');
-     Hintlist.Add('Hint');
-     smallimgindex.Add(Pointer(i));
-   end;
-   for i := 0 to 14 do begin
-     IMList.add(AppDir + inttostr(i) + '.jpg');
-     Pathlist.add('Folder or whatever');
-     Captionlist.add('Caption');
-     Hintlist.Add('Hint');
-     smallimgindex.Add(Pointer(i));
-   end;
    for i := 14 downto 0 do begin
      IMList.add(AppDir + inttostr(i) + '.jpg');
      Pathlist.add('Folder or whatever');
@@ -276,6 +364,14 @@ begin
      Hintlist.Add('Hint');
      smallimgindex.Add(Pointer(i));
    end;
+   for i := 0 to 14 do begin
+     IMList.add(AppDir + inttostr(i) + '.jpg');
+     Pathlist.add('Folder or whatever');
+     Captionlist.add('Caption');
+     Hintlist.Add('Hint');
+     smallimgindex.Add(Pointer(i));
+   end;
+
   skfmFlowGallery.MaxZoomSize := trunc(ClientHeight / 2);
   skfmFlowGallery.AddImages(IMList,Captionlist,Pathlist, Hintlist, smallimgindex);
   finally
@@ -313,12 +409,60 @@ begin
   skfmFlowGallery.MaxZoomSize := trunc(ClientHeight / 2);
 end;
 
+procedure TfrmMain.rbPagesizeMouseDown(Sender: TObject; Button: TMouseButton;
+    Shift: TShiftState; X, Y: Single);
+begin
+  if SpinBox1.Value < 50 then SpinBox1.Value := 50;
+
+end;
+
 procedure TfrmMain.saiAnimatedLogoAnimationFinished(Sender: TObject);
 begin
   lytcontrols.Visible := True;
   lytContent.Visible := True;
   Fill.Color := $FFEBEEF1;
   fanFadeOutTransition.Enabled := True;
+  Timer1.Enabled := True;
+end;
+
+procedure TfrmMain.SpinBox1Change(Sender: TObject);
+begin
+  if not Assigned(skfmFlowGallery) then Exit;
+
+  if rbAnimspeed.IsChecked then
+    skfmFlowGallery.AnimationSpeed := Round(SpinBox1.Value)
+
+  else if rbGlowwidth.IsChecked then
+    skfmFlowGallery.GlowWidth := Round(SpinBox1.Value)
+
+  else if rbHotwidth.IsChecked then
+    skfmFlowGallery.HotTrackWidth := Round(SpinBox1.Value)
+
+  else if rbFontsize.IsChecked then
+    skfmFlowGallery.CaptionFont.Size := SpinBox1.Value   // Single, passt perfekt
+
+  else if rbCaptionAlpha.IsChecked then
+    skfmFlowGallery.CaptionAlpha := Round(SpinBox1.Value)
+
+  else if rbCaptionYOffset.IsChecked then
+    skfmFlowGallery.CaptionOffsetY := Round(SpinBox1.Value)
+
+  else if rbPagesize.IsChecked then
+    skfmFlowGallery.PageSize := Round(SpinBox1.Value)
+
+  else if rbstartingAngle.IsChecked then
+    skfmFlowGallery.StartingAngle := Round(SpinBox1.Value)
+
+ else if rbRoundEdges.IsChecked then
+    skfmFlowGallery.RoundEdges := Round(SpinBox1.Value)
+
+ else if rbRotateall.IsChecked then
+    skfmFlowGallery.PutAllToAngle(SpinBox1.Value);
+end;
+
+procedure TfrmMain.Timer1Timer(Sender: TObject);
+begin
+  Timer1.Enabled := False;
   InitGallery;
 end;
 
